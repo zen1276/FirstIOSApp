c388a1d42a2b5cc4751bb0eb5565579d0cf5a9fd
Van hoffman <zvan1276@740s-pl-119.local>
2018-10-09 08:44:24 -0600

    Added a button, and created the model folder with a file in it.

---
+++ FirstIOSApp/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSApp
+//
+//  Created by Van hoffman, Zendan on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
