1f203605507e58e31cab4e30ac44bdfc133c2364
Van hoffman <zvan1276@740s-pl-119.local>
2018-10-09 07:52:41 -0600

    created the project and the .gitignore

---
+++ FirstIOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstIOSApp
+//
+//  Created by Van hoffman, Zendan on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
