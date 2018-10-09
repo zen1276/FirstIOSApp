97f2ae73b286fa53782c5dc7809ccb162bf973e3
Van hoffman <zvan1276@740s-pl-119.local>
2018-10-09 08:54:43 -0600

    created a float variable for percent of: red, green, and blue. Added a return to UIColor

--- FirstIOSApp/Model/ColorTool.swift
+++ FirstIOSApp/Model/ColorTool.swift
@@ -11,5 +11,12 @@ import UIKit
 
 public class ColorTool
 {
-    
+    public func createRandomColor() -> UIColor
+    {
+        let redPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        let greenPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        let bluePercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        
+        return UIColor(red: redPercent, green: greenPercent, blue: bluePercent, alpha: CGFloat(100))
+    }
 }
